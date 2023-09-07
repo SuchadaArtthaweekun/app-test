9148a1f9ff7c59cf3bad5218fbc7d4f7b50554b3
SuchadaArtthaweekun <Feemills@gmail.com>
2023-09-07 14:27:16 +0700

    Revert "Revert "fetch data""
    This reverts commit 24fc252f6f5eec8072711aa7097226106a924dd3.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import BoxComponent from "./components/Box";
+import Basketball from "./api/api";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <BoxComponent/>
+        <Basketball/>
       </header>
     </div>
   );
