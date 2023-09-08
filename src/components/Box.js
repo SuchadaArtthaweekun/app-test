ffab84e1665a2b11c9267f080cf998d9d1a87a10
kiartisak klomprakhon <om410@hotmail.com>
2023-09-08 10:27:06 +0700

    add text in box

--- src/components/Box.js
+++ src/components/Box.js
@@ -4,8 +4,12 @@ import Button from '@mui/material/Button';
 
 export default function BoxComponent() {
   return (
-    <Box component="span" sx={{ p: 2, border: '2px solid grey', borderRadius:5 }}>
-      <Button>Hello</Button>
-    </Box>
+    <>
+      <h1>Text Message</h1>
+      <Box component="span" sx={{ p: 2, border: '2px solid grey', borderRadius: 5 }}>
+        <Button>Hello</Button>
+      </Box>
+    </>
+
   );
 }
