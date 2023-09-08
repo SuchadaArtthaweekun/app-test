4545f9ae622a227ad783079a36041a88210b5a97
panuwat <parnuwat298ket@gmail.com>
2023-09-08 16:24:58 +0700

    btr

--- src/components/button.js
+++ src/components/button.js
@@ -4,6 +4,7 @@ function Button() {
   return (
     <div>
       <h3>btn</h3>
+      <h3>btn</h3>
         <button onClick={alert}>prev</button>
         <button onClick={alert}>next</button>
         
