d878990c649cec0fb53bd3ce1a819d96a4dde434
Suchada19 <Feemills19@gmail.com>
2023-09-08 16:19:47 +0700

    btn h3

--- src/components/button.js
+++ src/components/button.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Button() {
   return (
     <div>
+      <h3>btn</h3>
         <button onClick={alert}>prev</button>
         <button onClick={alert}>next</button>
         
