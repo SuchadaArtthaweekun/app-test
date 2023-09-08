ffc54243d9cf8d1b32febbd9ec51df5f60c8540f
panuwat <parnuwat298ket@gmail.com>
2023-09-08 16:55:53 +0700

    ช่วยด้วย

--- src/components/button.js
+++ src/components/button.js
@@ -5,6 +5,8 @@ function Button() {
     <div>
       <h3>btn</h3>
       <h1>PTT</h1>
+      <h1>พี่โอมมมมมมมม ช่วยด้วยยยยย</h1>
+      <h1>พี่ฟีมมมมมมมม ช่วยด้วยยยยย</h1>
         <button onClick={alert}>prev</button>
         <button onClick={alert}>next</button>
         
