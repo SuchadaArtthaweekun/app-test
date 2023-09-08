a6b49604892c76ec0ac4cf3a1216385495ebbe3f
Suchada19 <Feemills19@gmail.com>
2023-09-08 15:29:20 +0700

    Revert "Merge branch 'feature/button' of https://github.com/SuchadaArtthaweekun/app-test into feature/button"
    This reverts commit 1221a77f62c2309787b7a5e1352c890145ebf87c, reversing
    changes made to 5ea908a3c04879a2d9df3bb80ad90a5b5f5e2d17.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import BoxComponent from "./components/Box";
 import Basketball from "./api/api";
-import Card from "./components/Card";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <header className="App-header">
         <BoxComponent/>
         <Basketball/>
-        <Card />
       </header>
     </div>
   );
